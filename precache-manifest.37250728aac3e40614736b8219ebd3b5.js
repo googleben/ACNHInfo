d7a80707c08d3e12f7fbbcfaa2507350926f7a29
GoogleBen <william.ben.wyatt@gmail.com>
2020-04-07 21:06:04 -0500

    Updates

---
+++ precache-manifest.37250728aac3e40614736b8219ebd3b5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7e1c776aaf6a73f2a27ae1ab0eaf9e2",
+    "url": "/ACNHInfo/index.html"
+  },
+  {
+    "revision": "d900e4ebd4fc780d5627",
+    "url": "/ACNHInfo/static/css/main.a3155ce0.chunk.css"
+  },
+  {
+    "revision": "12ee73b67e652c893075",
+    "url": "/ACNHInfo/static/js/2.203f6956.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ACNHInfo/static/js/2.203f6956.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d900e4ebd4fc780d5627",
+    "url": "/ACNHInfo/static/js/main.77d0b794.chunk.js"
+  },
+  {
+    "revision": "0419ad034cc07d1e7fd9",
+    "url": "/ACNHInfo/static/js/runtime-main.d8e45b96.js"
+  },
+  {
+    "revision": "5ae22398ba18cf9e0abc50e6e74f8dc5",
+    "url": "/ACNHInfo/static/media/ACNH.5ae22398.eot"
+  },
+  {
+    "revision": "6e39a897bfb367bf743172a641707fa8",
+    "url": "/ACNHInfo/static/media/ACNH.6e39a897.svg"
+  },
+  {
+    "revision": "87f0deaa34983e6164aba47d11d68e9b",
+    "url": "/ACNHInfo/static/media/ACNH.87f0deaa.woff"
+  },
+  {
+    "revision": "b84202f613e02015eedf26d75a66a13d",
+    "url": "/ACNHInfo/static/media/ACNH.b84202f6.ttf"
+  }
+]);
